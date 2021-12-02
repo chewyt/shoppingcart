bf87b6f7ec03ce15d009506f1889e68254e96ce4
chewyt <yeetian.chew@gmail.com>
2021-12-02 14:11:49 +0800

    New console program for Shopping cart replacing the previous tutorial on basic console commands

--- src/main/java/chewyt/App.java
+++ src/main/java/chewyt/App.java
@@ -1,37 +1,105 @@
 package chewyt;
 
+import java.util.ArrayList;
 import java.io.Console;
-import java.util.logging.ConsoleHandler;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        
+        ArrayList<String> cart  = new ArrayList<String>();
         Console cons = System.console();
         String input;
-        do{
-            input = cons.readLine("Whats your hobby?");
-            input=input.trim().toLowerCase();
         
+        do{
 
-            switch (input){
+            input = cons.readLine("Welcome to your shopping cart\n");
 
-                case "swim" : System.out.println("The nearest pool is at my home");
-                break;
-                case "jog" : System.out.println("How far can you jog in your longest marathon?");
-                break;
-                case "code" : System.out.println("Lets be the fellow bro!");
-                break;
-                case "exit" : 
-                break;
-                default: System.out.printf("What is this %s hobby of yours?\n", input);
-                break;
+            String inputline = input.trim().toLowerCase();
+            // FOR checking parse strings | System.out.println("Commandline: "+inputline);
+
+            //regex used to ensure correct input is processed
+            Pattern add_pattern = Pattern.compile("^add ([a-zA-Z, ]+)");
+            Matcher add_matcher = add_pattern.matcher(inputline);
+            Pattern del_pattern = Pattern.compile("^delete ([1-9][0-9]*)");
+            Matcher del_matcher = del_pattern.matcher(inputline);
+
+            if (add_matcher.find()){
+
+                String itemlist = add_matcher.group(1);
+                add(itemlist,cart);
+                continue;
             }
-        }while(!input.toLowerCase().equals("exit"));
+
+            if (inputline.equals("list"))
+            {
+                display(cart);
+                continue;
+            }
+
+            if (del_matcher.find()){
+
+                Integer index = Integer.parseInt(del_matcher.group(1));
+                delete(cart, index);
+                continue;
+
+            }
+            // catching incorrect commands other than exit
+
+            if(!(input.equals("exit"))){
+                System.out.println("ERROR in console input: "+inputline);
+            }
+        }while(!(input.equals("exit")));
     }
+    public static void add(String itemlist, ArrayList<String> cart){
+        if(itemlist.contains(","))
+                {
+                    String items[] = itemlist.split(",");
+                    for (String i : items){
+                        if(!cart.contains(i.trim())) {
+                            cart.add(i.trim());
+                            System.out.println(i.trim()+" added to cart");
+                        }
+                        else{
+                            System.out.println("You have "+i.trim()+" in your cart");
+                        }
+                    }
+                }
+                else{
+                    if(!cart.contains(itemlist.trim())){
+                        cart.add(itemlist.trim());
+                        System.out.println(itemlist.trim()+" added to cart");
+                    }
+                    else{
+                        System.out.println("You have "+itemlist.trim()+" in your cart");
+                    }
+                }
+
+    }
+    public static void display(ArrayList<String> cartitems){
+
+        if (cartitems.size()==0){
+            System.out.println("Your cart is empty");
+        }
+        else {
+            for (int i = 1 ; i <= cartitems.size() ; i++) {
+                System.out.println(i+". "+cartitems.get(i-1));
+                }
+        } 
+    }
+
+    public static void delete(ArrayList<String> cartitems, int index){
+
+        if (index <= cartitems.size()){
+            System.out.println(cartitems.get(index-1)+" removed from cart");
+            cartitems.remove(index-1);
+        }
+        else {
+            System.out.println("Incorrect item index");
+        } 
+    }
+
+
+
 }
